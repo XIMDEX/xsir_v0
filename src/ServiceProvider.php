1312c45b56e5739612a349d9a34adb8616e79d1a
Alejandro de Tovar <venespana159@gmail.com>
2018-11-29 16:43:29 +0100

    Init project

---
+++ src/ServiceProvider.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Ximdex;
+
+use Illuminate\Support\ServiceProvider as LaravelServiceProvider;
+
+class ServiceProvider extends LaravelServiceProvider
+{
+    /**
+     * The event listener mappings for the application.
+     *
+     * @var array
+     */
+    protected $listen = [];
+
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+    }
+}
